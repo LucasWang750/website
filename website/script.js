2b26d1f3ea0407175d46e69db983f31b6a5384d0
LucasWang750 <47483544+LucasWang750@users.noreply.github.com>
2020-01-05 18:07:11 -0800

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,12 @@
+function changeWebpage(id){
+	document.location.href = "#description";
+}
+
+/*var vHeight = $(window).height(),
+    vWidth = $(window).width(),
+    cover = $('.cover');
+
+cover.css({"height":vHeight,"width":vWidth});
+*/
+
+window.scrollBy({top:100, left:0 , behavior:'smooth'});
